ea069203e2ba1be7c25618f5bdbfd3b20abf1bef
bryan-c-u <uranpvabryancamilo@gmail.com>
2026-08-30 23:23:40 -0500

    feat : subir estructura del microservicio de suscripciones

---
+++ src/test/java/com/inklusport/subscriptions/SubscriptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inklusport.subscriptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
