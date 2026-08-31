485fd64b487b02df708dcdcc94d8855bf76917a1
bryan-c-u <uranpvabryancamilo@gmail.com>
2026-08-31 01:09:21 -0500

    feat : agregar el archivo PagoEvento a la carpeta dto

---
+++ src/main/java/com/inklusport/subscriptions/dto/PagoEventoResponse.java
@@ -0,0 +1,27 @@
+package com.inklusport.suscripciones.dto;
+
+import com.inklusport.suscripciones.enums.EstadoPago;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PagoEventoResponse {
+    private Long id;
+    private String usuarioId;
+    private String eventoId;
+    private BigDecimal monto;
+    private String metodoPago;
+    private String referenciaTransaccion;
+    private EstadoPago estado;
+    private LocalDateTime fechaPago;
+    private Long comprobanteId;
+    private String numeroComprobante;
+}
