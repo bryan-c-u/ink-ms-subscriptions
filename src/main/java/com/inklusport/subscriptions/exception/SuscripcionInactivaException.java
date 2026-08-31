1616558161991b0dd089a4adde3cd4cfd5fb3d01
bryan-c-u <uranpvabryancamilo@gmail.com>
2026-08-31 01:03:16 -0500

    feat : agregar el archivo SuscripcionInactiva a la carpeta exception

---
+++ src/main/java/com/inklusport/subscriptions/exception/SuscripcionInactivaException.java
@@ -0,0 +1,7 @@
+package com.inklusport.suscripciones.exception;
+
+public class SuscripcionInactivaException extends RuntimeException {
+    public SuscripcionInactivaException(String message) {
+        super(message);
+    }
+}
